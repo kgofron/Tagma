36dab24db4efb2b787c0388035597768a4b556ab
Kaz Gofron <kgofron@comcast.net>
2020-05-18 17:44:55 -0400

    .gitignore tracked

--- op/disable.m
+++
@@ -1,8 +0,0 @@
-function disable ( chan )
-
-  disa=caget(x6b:det1.CHEN");
-  disa(chan)=1;
-  caput("x6b:det1.CHEN",disa);
-
-endfunction
-
--- op/enable.m
+++
@@ -1,8 +0,0 @@
-function enable ( chan )
-
-  disa=caget("x6b:det1.CHEN");
-  disa(chan)=0;
-  caput("x6b:det1.CHEN",disa);
-
-endfunction
-
